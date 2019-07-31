2a0d3483f19b80a602fe38ccaea50b8717d2a3af
Greygrz <gabdu74@gmail.com>
2019-07-31 18:38:22 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,100 @@
+$('#rigal').click(function(){
+    var audio = $("#audioperso1")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#alame').click(function(){
+    var audio = $("#audioperso2")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#lena').click(function(){
+    var audio = $("#audioperso3")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#tim').click(function(){
+    var audio = $("#audioperso4")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#arthur').click(function(){
+    var audio = $("#audioperso5")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#clement').click(function(){
+    var audio = $("#audioperso6")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#florent').click(function(){
+    var audio = $("#audioperso7")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#agathe').click(function(){
+    var audio = $("#audioperso8")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#tornier').click(function(){
+    var audio = $("#audioperso9")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#marine').click(function(){
+    var audio = $("#audioperso10")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#niels').click(function(){
+    var audio = $("#audioperso11")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#louac').click(function(){
+    var audio = $("#audioperso12")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#juliette').click(function(){
+    var audio = $("#audioperso13")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#philippe').click(function(){
+    var audio = $("#audioperso14")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#julien').click(function(){
+    var audio = $("#audioperso15")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#cyril').click(function(){
+    var audio = $("#audioperso16")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#leo').click(function(){
+    var audio = $("#audioperso17")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#jules').click(function(){
+    var audio = $("#audioperso18")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#clementine').click(function(){
+    var audio = $("#audioperso19")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
+$('#semoule').click(function(){
+    var audio = $("#audioperso20 ")[0];
+    audio.volume = 0.5;
+    audio.play();
+});
